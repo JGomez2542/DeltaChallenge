502effb025433f2283beadeff581d9e83cbdbe70
Jason <gomezja@plu.edu>
2018-09-15 10:55:18 -0400

    Testing changes

--- app/src/test/java/com/example/admin/deltachallenge/MainViewModelUnitTest.kt
+++ app/src/test/java/com/example/admin/deltachallenge/MainViewModelUnitTest.kt
@@ -1,27 +1,41 @@
 package com.example.admin.deltachallenge
 
 import android.arch.lifecycle.Observer
+import com.example.admin.deltachallenge.data.repository.Repository
 import com.example.admin.deltachallenge.ui.MainViewModel
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
+import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito.verify
+import org.mockito.Spy
+import org.mockito.junit.MockitoJUnitRunner
 
-class MainViewModelUnitTest(var mainViewModel: MainViewModel) {
+@RunWith(MockitoJUnitRunner::class)
+class MainViewModelUnitTest {
 
     @Mock
     lateinit var observer: Observer<List<Int>>
-
-    lateinit var numberList: List<Int>
-
-    @Before
-    fun setUp() {
-        numberList = emptyList()
-    }
+    @Mock
+    lateinit var argumentCaptor: ArgumentCaptor<List<Int>>
+    @Spy
+    lateinit var context: AppController
+    @Mock
+    lateinit var repository: Repository
+    @InjectMocks
+    lateinit var mainViewModel: MainViewModel
 
     @Test
     fun listLiveData_isUpdated() {
         mainViewModel.getRandomNumbers().observeForever(observer)
-        verify(observer).onChanged(numberList)
+        verify(observer).onChanged(argumentCaptor.capture())
+    }
+
+    @After
+    fun tearDown() {
+        mainViewModel.getRandomNumbers().removeObserver(observer)
     }
 }
